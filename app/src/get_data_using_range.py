00008b5db5d6b00253b7b2f05358c11a252c7875
Darshan Crout <dtcrout@gmail.com>
2018-07-03 19:49:33 -0400

    Clean up code style

--- app/src/get_data_using_range.py
+++ app/src/get_data_using_range.py
@@ -1,42 +1,41 @@
-import os
-import sys
-import json
-import datetime
-import time
-import requests
+"""get_data_using_range.py"""
 
 import argparse
+import datetime
+import json
+import os
+import requests
+import sys
+import time
 
-parser = argparse.ArgumentParser(description="Download APOD metadata using APOD API service.")
-
-parser.add_argument(
-    "conf",
-    type=argparse.FileType('r'),
-    default=sys.stdin,
-    help="path to configuration file"
+parser = argparse.ArgumentParser(
+    description='Download APOD metadata using APOD API service.'
 )
 
-parser.add_argument(
-    "-s", "--start-date",
-    default="1995-06-16",
-    help="start date in YYYY-MM-DD format"
-)
+parser.add_argument('conf',
+                    type=argparse.FileType('r'),
+                    default=sys.stdin,
+                    help='path to configuration file')
+
+parser.add_argument('-s',
+                    '--start-date',
+                    default='1995-06-16',
+                    help='start date in YYYY-MM-DD format')
 
 # Defaults to today's date
-parser.add_argument(
-    "-e", "--end-date",
-    default=datetime.datetime.today().strftime("%Y-%m-%d"),
-    help="end date in YYYY-MM-DD format"
-)
+parser.add_argument('-e',
+                    '--end-date',
+                    default=datetime.datetime.today().strftime('%Y-%m-%d'),
+                    help='end date in YYYY-MM-DD format')
 
 def main():
     args = parser.parse_args()
 
     # Read the configuration file
-    with open(args.conf.name, "r") as conf_file:
+    with open(args.conf.name, 'r') as conf_file:
         conf = json.load(conf_file)
     if not conf:
-        raise SystemExit("Invalid Configuration File")
+        raise SystemExit('Invalid Configuration File')
 
     # Read start and end dates
     earliest_day = args.start_date
@@ -45,15 +44,15 @@ def main():
     # Paths initialization
     src_dir = os.path.dirname(os.path.abspath(__file__))
     app_dir = os.path.dirname(src_dir)
-    metadata_dir = os.path.join(app_dir, conf["metadata_dir"])
+    metadata_dir = os.path.join(app_dir, conf['metadata_dir'])
 
     # API key
-    data_gov_api_key = os.environ.get("DATA_GOV_API_KEY")
+    data_gov_api_key = os.environ.get('DATA_GOV_API_KEY')
     if not data_gov_api_key:
-        raise ValueError("API key not found.\n`export DATA_GOV_API_KEY=<your_API_key>` before running this program")
+        raise ValueError('API key not found.\n`export DATA_GOV_API_KEY=<your_API_key>` before running this program')
 
     # API end-point
-    api_endpoint = conf["apod_api_url"]
+    api_endpoint = conf['apod_api_url']
 
     # Starting and ending years
     earliest_year = int(earliest_day[:4])
@@ -63,31 +62,30 @@ def main():
     for yyyy in range(earliest_year, latest_year+1):
         if yyyy == earliest_year:
             start_date = earliest_day
-            end_date = "{}-12-31".format(earliest_year)
+            end_date = '{}-12-31'.format(earliest_year)
         elif yyyy == latest_year:
-            start_date = "{}-01-01".format(latest_year)
+            start_date = '{}-01-01'.format(latest_year)
             end_date = latest_day
         else:
-            start_date = "{}-01-01".format(yyyy)
-            end_date = "{}-12-31".format(yyyy)
+            start_date = '{}-01-01'.format(yyyy)
+            end_date = '{}-12-31'.format(yyyy)
 
-        payload = {
-            "api_key" : data_gov_api_key,
-            "start_date" : start_date,
-            "end_date" : end_date
-        }
+        payload = {"api_key": data_gov_api_key,
+                   "start_date": start_date,
+                   "end_date": end_date}
 
         r = requests.get(api_endpoint, payload)
 
         if r.status_code != 200:
-            print("Could not get data for year {}".format(yyyy))
+            print('Could not get data for year {}'.format(yyyy))
             continue
 
         response_json = r.json()
-        outfile = os.path.join(metadata_dir, "metadata_{}".format(yyyy))
+        outfile = os.path.join(metadata_dir, 'metadata_{}'.format(yyyy))
 
-        print("Year: {} - Writing {} records to {}".format(yyyy, len(response_json), outfile))
-        with open(outfile, "w") as o:
+        print('Year: {} - Writing {} records to {}'\
+            .format(yyyy, len(response_json), outfile))
+        with open(outfile, 'w') as o:
             json.dump(response_json, o)
         time.sleep(1)
 
--- app/src/get_images.py
+++ app/src/get_images.py
@@ -1,71 +1,65 @@
-import os
-import sys
-import json
-import requests
-from time import sleep
+"""get_images.py"""
 
 import argparse
+import json
+import os
+import requests
+import sys
+from time import sleep
 
 parser = argparse.ArgumentParser(description='Download APID dataset images.')
 
-parser.add_argument(
-    "conf",
-    type=argparse.FileType('r'),
-    default=sys.stdin,
-    help="path to configuration file"
-)
+parser.add_argument('conf',
+                    type=argparse.FileType('r'),
+                    default=sys.stdin,
+                    help='path to configuration file')
 
 def save_image_from_url(url, download_dir):
-    """
-    Saves image at the given URL to file system
-    """
+    """Saves image at the given URL to file system."""
     # Get the filename from URL
     filename = os.path.basename(url)
 
     # Check if file has already been downloaded
     if filename in os.listdir(download_dir):
-        print("Image already exists: {}".format(filename))
+        print('Image already exists: {}'.format(filename))
         return True
-
 
     r = requests.get(url, stream=True)
     if r.status_code == 200:
         success = True
         # Download the binary stream in chunks
-        with open(os.path.join(download_dir,filename), "wb") as image_file:
+        with open(os.path.join(download_dir,filename), 'wb') as image_file:
             for chunk in r:
                 image_file.write(chunk)
     else:
         success = False
 
-    print("Download Success: {}; URL: {}".format(success, url))
-    # sleep for a second before making the next request
+    print('Download Success: {}; URL: {}'.format(success, url))
+    # Sleep for a second before making the next request
     sleep(1)
     return success
 
 def main():
-    """
-    Reads the data and saves the images
-    """
+    """Reads the data and saves the images."""
     # Read configuration file
     conf = None
     args = parser.parse_args()
-    with open(args.conf.name, "r") as conf_file:
+    with open(args.conf.name, 'r') as conf_file:
         conf = json.load(conf_file)
 
     if not conf:
-        raise SystemExit("Invalid Configuration File")
+        raise SystemExit('Invalid Configuration File')
 
     # Paths initialization
     src_dir = os.path.dirname(os.path.abspath(__file__))
     app_dir = os.path.dirname(src_dir)
-    metadata_dir = os.path.join(app_dir, conf["metadata_dir"])
-    download_dir = os.path.join(app_dir, conf["download_dir"])
+    metadata_dir = os.path.join(app_dir, conf['metadata_dir'])
+    download_dir = os.path.join(app_dir, conf['download_dir'])
 
     # Read available data from metadata directory
     data = []
-    for f in os.listdir(conf["metadata_dir"]):
-        with open(os.path.join(metadata_dir, f), "r") as json_data:
+    for f in os.listdir(conf['metadata_dir']):
+        with open(os.path.join(metadata_dir, f), 'r') as json_data:
             data += json.load(json_data)
 
     # Use only non-copyright protected data
@@ -75,22 +69,25 @@ def main():
 
     # Use only image data
     image_data = list(
-        filter(lambda p: p if p["media_type"] == "image" else None, non_copyright_data)
+        filter(lambda p: p if p["media_type"]=="image" else None,
+               non_copyright_data)
     )
 
     # Print statistics
-    print("Total Items:             {}".format(len(data)))
-    print("Public Domain Items:     {}".format(len(non_copyright_data)))
-    print("Public Domain Images:    {}".format(len(image_data)))
+    print('Total Items:             {}'.format(len(data)))
+    print('Public Domain Items:     {}'.format(len(non_copyright_data)))
+    print('Public Domain Images:    {}'.format(len(image_data)))
 
     # Download Images
     for d in image_data:
-        url_keys = set(["url", "hdurl"])
+        url_keys = set(['url', 'hdurl'])
         for image in image_data:
             # See which keys are present in the JSON
             found_keys = filter(lambda url_key: url_key in url_keys, image.keys())
             # Save all the images
-            list(map(lambda k: save_image_from_url(k, download_dir), list(map(lambda k: image[k], found_keys))))
+            list(map(lambda k: save_image_from_url(k, download_dir),
+                 list(map(lambda k: image[k], found_keys))))
+
 
 if __name__ == "__main__":
     main()
